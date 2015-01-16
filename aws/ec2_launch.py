db1d945967e40960a140f9d61375b3b0e8e9adbc
Yexing <yexing@edanzgroup.com>
2015-01-16 18:09:09 +0800

    modified:   ec2_launch.py
            1. Rename script according to our roadmap.
            2. Refine script to run as main script.
            3. Add argument ami support customize ami
            4. Add argument file support customize file
            5. Add argument debug support enable debug mode
            6. Refine tags as common_tags and ec2_special_tags to codinate
            with our schedule run script.
            7. Add cli_version so we can track the resource be launch by
            which cli version.
            8. change default ebs size to 20G to match our use case

--- aws/ec2_launch.py
+++ aws/ec2_launch.py
@@ -4,6 +4,8 @@
 # Auto add/remove cloudwatch
 # Auto config dynamic dns using aws global route53 ttl to 60 seconds 
 
+import sys
+import argparse
 import os
 import os.path
 import socket
@@ -12,11 +14,13 @@ import base64
 import boto.ec2
 
 #Functions:
+
+#Any Bootstrip?
 def bootstrip(instance):
     print "Bootstrip instance.."
     time.sleep(1)
 
-#Function - Check port reachable
+#Check port reachable
 def check_port(public_dns_name, port=22, timeout=5):
     print "Check Server %s Port %i .. (Timeout is %i seconds)" % (public_dns_name, port, timeout)
     try_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,91 +35,114 @@ def check_port(public_dns_name, port=22, timeout=5):
         print "Error on connect: %s" % e
         try_socket.close()
         return 1
+
+def main():
+    parser = argparse.ArgumentParser(description='Edanz AWS CLI Tool - Launch Base AMI or your own specifiy AMI')
+    parser.add_argument('-a', '--ami', required=True, help='The AMI you want to launched')
+    parser.add_argument('-f', '--file', type=argparse.FileType('r'), help='The file to provide user-data')
+    parser.add_argument('-v', '--version', default=0.1, action='version', version='%(prog)s 0.1')
+    parser.add_argument('--debug', type=int, default=0, choices=[0,1], help='--debug 1 enable debug mode')
+    args = parser.parse_args()
+
+    #Some Common Vars:
+    USER = os.environ['USER']
+
+    #Read user-data File: 
+    bootstrip_file_contents = args.file.read()
+    args.file.close()
+
+    #Get time:
+    time_display = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    #Connect to region with profile 'cn'
+    if args.debug == 0 :
+        conn = boto.ec2.connect_to_region('cn-north-1',profile_name='cn')
+    else:
+        conn = boto.ec2.connect_to_region('cn-north-1',profile_name='cn',debug=2)
+
+    #Defined Tags:
+    #TODO: using config file?
+    common_tags= {
+            'Name': "DailyDev Resource Launch by " + USER ,
+            'application': 'sandbox',
+            'environment': 'dev',
+            'role': 'dev-coding',
+            'createtime': time_display,
+            'cli_version': args.version,
+            'owner': USER
+            }
+    ec2_special_tags = {
+            'auto:start':'20 09 * * 1-5',
+            'auto:stop':'30 19 * * *',
+            }
+
+
+    #Define the network interface will be used inside VPC:
+    #TODO: auto select subnet.
+    network_interface_config = boto.ec2.networkinterface.NetworkInterfaceSpecification(
+            subnet_id                   = 'subnet-cc8b9bae',
+            groups                      = ['sg-f59a8497'],
+            description                 = "Created by Edanz CLI: ec2.py",
+            delete_on_termination       = True,
+            associate_public_ip_address = True
+            )
+    network_interface = boto.ec2.networkinterface.NetworkInterfaceCollection(network_interface_config)
+
+    #Define the block device:
+    dev_sda1 = boto.ec2.blockdevicemapping.BlockDeviceType()
+    dev_sda1.size = 20 
+    dev_sda1.delete_on_termination = True
+    #Add tag for block device:(not working)
+    #dev_sda1.add_tags(common_tags)
+
+    block_device_map = boto.ec2.blockdevicemapping.BlockDeviceMapping()
     
+    block_device_map['/dev/xvda'] = dev_sda1
+
+    #Launch instances into VPC:
+    reservation = conn.run_instances(
+                                    max_count            = 1,
+                                    key_name             = 'AWS_CN_General',
+                                    image_id             = args.ami,
+                                    instance_type        = 't2.micro',
+                                    instance_profile_arn = 'arn:aws-cn:iam::153162420102:instance-profile/DevDaily',
+                                    user_data            = base64.b64encode(bootstrip_file_contents),
+                                    block_device_map     = block_device_map,
+                                    network_interfaces   = network_interface,
+                                    )
+
+
+    #Get the instance object:
+    instance = reservation.instances[0]
+
+    #Add Tag for the instances:
+    instance.add_tags(common_tags)
+    instance.add_tags(ec2_special_tags)
+
+    #Wait instances to ready for use: 
+    time.sleep(5)
+    instance.update()
+    while instance.state == "pending":
+        print "Instance %s statue is %s, waiting instances state to become running.." % (instance.id, instance.state)
+        time.sleep(5)
+        instance.update()
+
+    #Check SSH Connection:
+    result = check_port(instance.public_dns_name, 22)
+    while result == 1:
+        time.sleep(5)
+        result = check_port(instance.public_dns_name, 22)
+
+    #Bootstrip:
+    bootstrip(instance)
+
+    #Output instances info:
+    print "instance %s Ready! please login here:" % (instance.id)
+    print instance.public_dns_name
+        
 
 #Functions end
 
-#Some Common Vars:
-SCRIPT_NAME = os.path.basename(__file__)
-USER = os.environ['USER']
-BOOTSTRIP_FILE_NAME = './bootstrip.sh'
-
-#Read Bootstrip File: 
-BOOTSTRIP_FILE = open(BOOTSTRIP_FILE_NAME,'r')
-bootstrip_file_contents = BOOTSTRIP_FILE.read()
-BOOTSTRIP_FILE.close()
-
-#Define:
-#AWS China HVM: Amazon Linux AMI release 2014.09
-ami = 'ami-ce46d4f7'
-
-#Connect to region with profile 'cn'
-conn = boto.ec2.connect_to_region('cn-north-1',profile_name='cn')
-#conn = boto.ec2.connect_to_region('cn-north-1',profile_name='cn',debug=2)
-
-#Define the network interface will be used inside VPC:
-network_interface_config = boto.ec2.networkinterface.NetworkInterfaceSpecification(
-        subnet_id                   = 'subnet-cc8b9bae',
-        groups                      = ['sg-f59a8497'],
-        description                 = "Created by Edanz CLI: ec2.py",
-        delete_on_termination       = True,
-        associate_public_ip_address = True
-        )
-network_interface = boto.ec2.networkinterface.NetworkInterfaceCollection(network_interface_config)
-
-
-#Define the block device:
-dev_sda1 = boto.ec2.blockdevicemapping.BlockDeviceType()
-dev_sda1.size = 10 
-dev_sda1.delete_on_termination = True
-
-block_device_map = boto.ec2.blockdevicemapping.BlockDeviceMapping()
-block_device_map['/dev/xvda'] = dev_sda1
-
-#Launch instances into VPC:
-reservation = conn.run_instances(
-                                max_count            = 1,
-                                key_name             = 'AWS_CN_General',
-                                image_id             = ami,
-                                instance_type        = 't2.micro',
-                                instance_profile_arn = 'arn:aws-cn:iam::153162420102:instance-profile/DevDaily',
-                                user_data            = base64.b64encode(bootstrip_file_contents),
-                                block_device_map     = block_device_map,
-                                network_interfaces   = network_interface,
-                                )
-
-
-#Get the instance object:
-instance = reservation.instances[0]
-
-#Add Tag for the instances:
-tags= {
-        'Name': "DailyDev Machine Launch by " + USER ,
-        'application':'sandbox',
-        'environment':'dev',
-        'role':'dev-testing',
-        'owner': USER
-        }
-instance.add_tags(tags)
-
-#Wait instances to ready for use: 
-time.sleep(5)
-instance.update()
-while instance.state == "pending":
-    print "Instance %s statue is %s, waiting instances state to become running.." % (instance.id, instance.state)
-    time.sleep(5)
-    instance.update()
-
-#Check SSH Connection:
-result = check_port(instance.public_dns_name, 22)
-while result == 1:
-    time.sleep(5)
-    result = check_port(instance.public_dns_name, 22)
-
-#Bootstrip:
-bootstrip(instance)
-
-#Output instances info:
-print "instance %s Ready! please login here:" % (instance.id)
-print instance.public_dns_name
+if __name__ == "__main__":
+    main()
 
